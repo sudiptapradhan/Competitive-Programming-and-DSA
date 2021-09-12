ede28d34258ae0b184111ad83f2040866b2af152
Sudipta Pradhan <64891121+sudiptapradhan@users.noreply.github.com>
2021-09-12 20:20:24 +0530

    updated Soln 1

--- Questions/week 3/solution_1.java
+++ Questions/week 3/solution_1.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
 public class solution_1 {
-         int arr[] = new int[]{1,2,3,4};
+         
          public static void rotate(int arr[], int n){
             int temp = arr[n-1];
             for (int i = n-1; i > 0; i--){
